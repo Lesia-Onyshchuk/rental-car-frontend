00ce53865bde7cfe5e52f7d599894e519f042fd5
lesia_onyshchuk <lkane443@gmail.com>
2025-06-13 18:08:19 +0300

    pagination

--- src/redux/cars/slice.js
+++ src/redux/cars/slice.js
@@ -6,9 +6,9 @@ const initialState = {
   car: null,
   loading: false,
   error: null,
-  totalCars: "",
-  page: "",
-  totalPages: "",
+  totalCars: 0,
+  page: 1,
+  totalPages: 0,
 };
 
 const slice = createSlice({
@@ -20,8 +20,15 @@ const slice = createSlice({
         state.loading = true;
       })
       .addCase(fetchCars.fulfilled, (state, action) => {
-        state.items = action.payload;
         state.loading = false;
+
+        state.page = Number(action.payload.page);
+        state.items =
+          state.page > 1
+            ? [...state.items, ...action.payload.cars]
+            : action.payload.cars;
+        state.totalCars = action.payload.totalCars;
+        state.totalPages = action.payload.totalPages;
       })
       .addCase(fetchCars.rejected, (state, action) => {
         (state.loading = false), (state.error = action.payload);
