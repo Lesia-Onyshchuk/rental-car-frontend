3efd67360f3f5fea5f4ae425a52b189f8fe95ab4
lesia_onyshchuk <lkane443@gmail.com>
2025-06-15 22:50:55 +0300

    fix loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Loader from "./components/Loader/Loader.jsx";
 import "./App.css";
 import { lazy, Suspense } from "react";
 import Layout from "./Layout.jsx";
+import { useSelector } from "react-redux";
+import { selectLoading } from "./redux/cars/selectors.js";
 
 const Home = lazy(() => import("./pages/Home/Home.jsx"));
 const Catalog = lazy(() => import("./pages/Catalog/Catalog.jsx"));
@@ -10,8 +12,10 @@ const Car = lazy(() => import("./pages/Car/Car.jsx"));
 const NotFound = lazy(() => import("./pages/NotFound/NotFound.jsx"));
 
 const App = () => {
+  const isLoading = useSelector(selectLoading);
   return (
     <div className="appBox">
+      {isLoading && <Loader />}
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Layout />}>
